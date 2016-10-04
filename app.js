af5142e53b6389c257122f057be3348288887722
Matt Bedell <matt@bedell.co>
2016-10-04 15:37:45 -0400

    removed move function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log('Script Loaded.');
      this.element = $('.movSprite');
      this.speed = 20;
      this.origOffset = this.element.offset();
-     this.element.css({'background-image': 'url(assets/pacStart.png)', 'background-size': '100% 100%'});//Initial background image for sprite
+     this.element.css('background-image', 'url(assets/pacStart.png');//Initial background image for sprite
      this.assetLibrary = {
         leftI: ['url(assets/pacStart.png)', 'url(assets/pacLeft2.png)', 'url(assets/pacLeft3.png)'],
         rightI: ['url(assets/pacStart.png)', 'url(assets/pacRight2.png)', 'url(assets/pacRight3.png)'],
@@ -13,47 +13,18 @@ console.log('Script Loaded.');
      }
      this.runArrowListener();
      this.imgCounter = 0;
+     this.direction = 39;
      this.position = {
         xP: this.origOffset.left,
         yP: this.origOffset.top
      }
-   }
-   updatePosition(axis, speed) {
-      switch(axis){
-         case 'x':
-            this.position.xP += speed;
-         break;
 
-         case 'y':
-            this.position.yP += speed;
-         break;
-      }
-      this.element.offset({top: this.position.yP, left: this.position.xP});
-   }
-   move(direction){
-      switch(direction){
-         case 38://UP
-            this.updatePosition('y', (this.speed * - 1));
-            this.animateMe('up');
-            break;
-         case 40://DOWN
-            this.updatePosition('y', this.speed);
-            this.animateMe('down');
-            break;
-         case 37://LEFT
-            this.updatePosition('x', (this.speed * - 1));
-            this.animateMe('left');
-            break;
-         case 39://RIGHT
-            this.updatePosition('x', this.speed );
-            this.animateMe('right');
-            break;
-      }
    }
    runArrowListener(){
       let that = this;
       $('body').on('keydown', function(e){
-         that.move(e.keyCode);
+         that.direction = e.keyCode;
+         console.log(that.direction);
       })
    }
    animateMe(direction){
