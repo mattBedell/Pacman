d786585976af0d066a3825c4977435b2eb555a57
Matt Bedell <matt@bedell.co>
2016-10-05 17:42:33 -0400

    fixed some weirdness with grabbing correct element in the grid, working on setting bounds in grid

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ class GridProperties {
          gUp: true,
          gDown: true,
       }
+      this.myEl;
    }
 }
-
- class MakeBoard {
+class MakeBoard {
    constructor(numberOfRows, numberOfColumns){
      this.gW = numberOfColumns;
      this.gH = numberOfRows;
      this.grid = {
         row: []
      }
-     this.myEl;
      this.makeGrid();
    }
    makeGrid(){
+      var divCounter = 0;
       for(var i = 0; i < this.gH; i++){
          var giveRowClass = 'row rowNum' + i;
          var newRow = '<div class="' + giveRowClass + '"></div>';
@@ -33,26 +33,36 @@ class GridProperties {
          for(var j = 0; j < this.gW; j++){
             var rowClass = ' br' + i;
             var colClass = ' bc' + j;
-            var numClass = 'nm' + i + j;
+            var numClass = 'nm' + divCounter;
             var newDiv = '<div class="gameBlock' + rowClass + colClass + ' ' + numClass + '"></div>';
             rowToAppend.append(newDiv);
             this.grid.row[i][j] = new GridProperties();
             this.grid.row[i][j].myEl = $('.' + numClass);
+            divCounter++;
          }
       }
    }
  }
- var gBoard = new MakeBoard(20, 20);
-
-
-
-
-
-
-
-
-
-
-
-
- //
+class MakeBounds {
+   constructor() {
+      this.outerBounds  = {
+         top: '',
+         right: '',
+         bottom: '',
+         left: ''
+      }
+   }
+   getBounds(){
+      var nextRow = 0;
+      var nextCol = 0;
+      for(var i = 0; i < gBoard.grid.row[0].length; i++) {
+         if(nextCol < gBoard.grid.row.length) {
+            gBoard.grid.row[0][nextCol].canMove.gUp = false;
+            gBoard.grid.row[0][nextCol].myEl.css('background-color', 'red');
+            nextCol++;
+         }
+      }
+   }
+}
+var bounds = new MakeBounds();
+var gBoard = new MakeBoard(20, 20);
