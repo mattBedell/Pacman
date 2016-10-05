54f86b3a273ca50dea8dad57f1d24c4e5069ad86
Matt Bedell <matt@bedell.co>
2016-10-05 15:37:03 -0400

    added dynamic board creation and grid lookup system

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 console.log("Script Loaded");
+class GridProperties {
+   constructor(element){
+      this.canMove = {
+         gLeft: true,
+         gRight: true,
+         gUp: true,
+         gDown: true,
+      }
+      this.element = element;
+   }
+}
+
  class MakeBoard {
-   constructor(){
-     this.gW = 20;
-     this.gH = 20;
-     //this.myGrid = [rows[], columns[]]
+   constructor(numberOfRows, numberOfColumns){
+     this.gW = numberOfColumns;
+     this.gH = numberOfRows;
+     this.grid = {
+        row: []
+     }
+     this.makeGrid();
    }
    makeGrid(){
       for(var i = 0; i < this.gH; i++){
@@ -12,22 +27,20 @@ console.log("Script Loaded");
          $('body').append(newRow);
          var myRow = '.rowNum' + i;
          var rowToAppend = $(myRow);
+         var colN = [];
+         this.grid.row[i] = colN;
 
-         for(var i = 0; i < this.gW; i++){
+         for(var j = 0; j < this.gW; j++){
             var rowClass = ' br' + i;
-            var colClass = ' bc' + i;
+            var colClass = ' bc' + j;
             var newDiv = '<div class="gameBlock' + rowClass + colClass + '"></div>';
             rowToAppend.append(newDiv);
-     }
+            this.grid.row[i][j] = new GridProperties($(newDiv));
+         }
+      }
    }
  }
- var myGrid = new MakeBoard();
- myGrid.makeGrid();
-
-
-
-
-
+ var gBoard = new MakeBoard(25, 25);
 
 
 
